7e7efaeb4d8984df21fa01be0212dc6dca1869c8
Jules ten Bos <jules.tenbos@gmail.com>
2025-06-09 03:33:13 +0800

    updated boot app

---
+++ release/install/boot/modules/tools/index.js
@@ -0,0 +1,6 @@
+//  name        The tools Package
+//  URI         tools
+//  type        Module Package
+//  description The tools package contains utility APIs and tools
+//              for various development and system operations.
+///////////////////////////////////////////////////////////////////////////////
